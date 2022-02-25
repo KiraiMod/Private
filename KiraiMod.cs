d00f5ff0b858f04209b721b44003c9ff73bd0a05
xKiraiChan <xKiraiChan@gmail.com>
2022-02-24 20:50:51 -0500

    split into private module

---
+++ KiraiMod.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using KiraiMod.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KiraiMod.Private
+{
+    [BepInPlugin("me.kiraihooks.KiraiMod.Private", "KM.Private", "0.0.0")]
+    public class KiraiMod : BasePlugin
+    {
+        AssetBundle bundle;
+
+        public override void Load()
+        {
+            Managers.ModuleManager.Register();
+            Managers.GUIManager.OnLoad += GUIManager_OnLoad;
+        }
+
+        private void GUIManager_OnLoad()
+        {
+            MemoryStream mem = new();
+            Assembly.GetExecutingAssembly().GetManifestResourceStream("KiraiMod.Private.Lib.KiraiMod.Private.GUI.AssetBundle").CopyTo(mem);
+            bundle = AssetBundle.LoadFromMemory(mem.ToArray());
+            bundle.hideFlags |= HideFlags.HideAndDontSave;
+
+            Transform GUI = bundle.LoadAsset("assets/private.gui.prefab")
+                .Cast<GameObject>()
+                .Instantiate()
+                .transform;
+
+            for (int i = 0; i < GUI.childCount; i++)
+                GUI.GetChild(i).SetParent(Managers.GUIManager.UserInterface.transform);
+
+            GUI.Destroy();
+        }
+    }
+}
